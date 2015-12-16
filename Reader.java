7b136c91a0662f3e0411990fe38410c6b59dae7c
Monark Modi <monark@umass.edu>
2015-12-15 20:35:01 -0500

    Updated Reader.java

--- Reader.java
+++ Reader.java
@@ -1,17 +1,67 @@
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 
-import java.io.*;
 
-public class Reader{
 
-public static void main(String[]args) throws IOException{
+public class Reader { 
+	
+	
+	InputStream fis = new FileInputStream("the_file_name");
+    InputStreamReader isr = new InputStreamReader(fis, Charset.forName("UTF-8"));
+	BufferedReader br = new BufferedReader(isr);
+	Scanner scan = new Scanner(System.in);
+    Scanner input = new Scanner(fis);
 
-    FileReader in = new FileReader("File Directory");
-    BufferedReader br = new BufferedReader(in);
 
-    while (br.readLine() != null) {
-        System.out.println(br.readLine());
-    }
-    in.close();
 
+public courseClass add(String courseClass){
+	
+	/*
+	FileUtils.writeStringToFile(file, courseClass, true);
+	*/
+
+	try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("file", true)))) {
+    out.println(courseClass);
+	}
+	catch (IOException e) {
+    System.err.println(e);
 }
 }
+
+public courseClass searchAndReturn(String courseCode){
+	
+	/*
+	FileUtils.readFileToString(file).contains(courseClass);
+	*/
+
+	 while ((courseCode=br.readLine()) != null) {
+     return courseCode;
+  }
+
+}
+
+public boolean search(String courseCode){
+
+	/*
+
+	if((courseCode=br.readLine() != null)){
+		return true;
+	}
+ 	else{
+	return false;
+	}
+
+	*/
+}
+
+
+}
+

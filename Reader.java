e40aeba816eaaaabcf3dd95940d0512d045063e8
Monark Modi <monark@umass.edu>
2015-12-15 21:15:28 -0500

    Updated Reader.java

--- Reader.java
+++ Reader.java
@@ -11,22 +11,57 @@ import java.io.FileInputStream;
 
 
 
-public class Reader 
-{ 
+public class Reader { 
+	
+	
+	InputStream fis = new FileInputStream("the_file_name");
+    InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+	BufferedReader br = new BufferedReader(isr);
+	Scanner scan = new Scanner(System.in);
+    Scanner input = new Scanner(fis);
 
-	public void add(CourseClass c)
-	{
+
+
+public CourseClass add(CourseClass c){
+	
+	/*
+	FileUtils.writeStringToFile(file, courseClass, true);
+	*/
+
+	try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("file", true)))) {
+    out.println(c);
+	}
+	catch (IOException e) {
+    System.err.println(e);
+}
+}
+
+public CourseClass searchAndReturn(String courseCode){
+	
+	/*
+	FileUtils.readFileToString(file).contains(courseClass);
+	*/
+
+	 while ((courseCode=br.readLine()) != null) {
+     return courseCode; //make changes here
+  }
+
+}
+
+public boolean search(String courseCode){
+
+	
+	// Also here!
+	if((courseCode=br.readLine() != null)){
+		return true;
+	}
+ 	else{
+	return false;
 	}
 
-	public CourseClass searchAndReturn(String courseCode)
-	{
-		CourseClass temp = new CourseClass("abc");
-		return temp;
-	}
+	
+}
 
-	public boolean search(String courseCode)
-	{
-	}
 
 }
 

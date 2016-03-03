e39c380b3b54af9237a3f307399a1e8edf90fe7a
Rachit Nigam <rnigam@umass.edu>
2016-03-03 12:41:32 -0500

    Removed useless code, moved repo into public

--- CourseClassNotFoundException.java
+++
@@ -1,5 +0,0 @@
-public class CourseClassNotFoundException extends Exception
-{
-	public CourseClassNotFoundException(String courseCode)
-	{super("Could not find " + courseCode+" in the database");}
-}
